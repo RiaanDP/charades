273d1c6a3a6126ad80d687287d1314cbf6702374
Riaan du Plessis <duplessis.riaan@gmail.com>
2026-01-28 00:43:52 +0400

    Remove tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,10 +8,6 @@ import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
@@ -26,8 +22,8 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="game" options={{ headerShown: false }} />
       </Stack>
       <StatusBar hidden />
     </ThemeProvider>
